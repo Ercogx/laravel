394f9001e0f0d076e2950946452149164082d265
Vitalik Zubchyk <ercogx@gmail.com>
2021-04-09 13:36:21 +0300

    Lesson 7 Seeds - fabric import demo content

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserTableSeeder extends Seeder
 {
@@ -13,6 +14,19 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name'      => 'Unknown author',
+                'email'     => 'author_unknow@g.g',
+                'password'  => bcrypt(Str::random())
+            ],
+            [
+                'name'      => 'Vitalik',
+                'email'     => 'author@g.g',
+                'password'  => bcrypt('111')
+            ]
+        ];
+
+        \DB::table('users')->insert($data);
     }
 }
