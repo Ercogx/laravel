5d9c7bb0f24bbf543f0f5d1946b764210dbe514e
Vitalik Zubchyk <ercogx@gmail.com>
2021-04-12 15:30:21 +0300

    Lesson 39, Edit article page blog #2

--- resources/views/blog/admin/posts/edit.blade.php
+++ resources/views/blog/admin/posts/edit.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+
     @php /** @var \App\Models\BlogPost $item */@endphp
 
     @include('blog.admin.posts.include.result_message')
@@ -12,8 +14,6 @@
         <form method="POST" action="{{ route('blog.admin.posts.store') }}">
     @endif
         @csrf
-
-        <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     @include('blog.admin.posts.include.post_edit_main_col')
@@ -22,6 +22,24 @@
                     @include('blog.admin.posts.include.post_edit_add_col')
                 </div>
             </div>
-        </div>
-    </form>
+        </form>
+
+        @if($item->exists)
+            <br>
+            <form method="POST" action="{{ route('blog.admin.posts.destroy', $item->id) }}">
+                @method('DELETE')
+                @csrf
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="card card-block">
+                            <div class="card-body ml-auto">
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        @endif
+
+    </div>
 @endsection
---
+++ resources/views/blog/admin/posts/include/post_edit_main_col.blade.php
@@ -0,0 +1,82 @@
+@php /** @var \App\Models\BlogPost $item */ @endphp
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                @if($item->is_published)
+                    Published
+                @else
+                    Draft
+                @endif
+            </div>
+            <div class="card-body">
+                <div class="card-title"></div>
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" role="tab" href="#maindata">Primary data</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" role="tab" href="#adddata">Additional data</a>
+                    </li>
+                </ul>
+                <br>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="maindata" role="tabpanel">
+
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input name="title" value="{{ $item->title }}" id="title" type="text"
+                                   class="form-control" minlength="3" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="content-raw">Article</label>
+                            <textarea name="content_raw" id="content-raw"
+                                      class="form-control" rows="20">{{ old('content_raw', $item->content_raw) }}</textarea>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="adddata" role="tabpanel">
+                        <div class="form-group">
+                            <label for="category_id">Category</label>
+                            <select name="category_id" id="category_id" type="text"
+                                    class="form-control">
+                                @foreach($categoryList as $categoryOption)
+                                    <option value="{{ $categoryOption->id }}"
+                                            @if($categoryOption->id === $item->category_id) selected @endif>
+                                        {{ $categoryOption->id_title }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="slug">Slug</label>
+                            <input name="slug" value="{{ $item->slug }}" id="slug" type="text"
+                                   class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="excerpt">Excerpt</label>
+                            <textarea name="excerpt" id="excerpt"
+                                      class="form-control" rows="5">{{ old('excerpt', $item->excerpt) }}</textarea>
+                        </div>
+
+                        <div class="form-check">
+                            <input type="hidden" name="is_published" value="0">
+
+                            <input type="checkbox"
+                                   id="is_published"
+                                   class="form-check-input"
+                                   value="1"
+                                   @if($item->is_published)
+                                       checked
+                                   @endif
+                            >
+                            <label for="is_published">Published</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
